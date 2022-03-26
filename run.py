548d613878ee114f3c54c73688ca2938ede4aa90
CCrocilla <cla.crocilla@gmail.com>
2022-03-26 00:32:02 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,7 +14,7 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figured input from the user
+    Get sales figures input from the user
     Run a while loop to collect a valid string of data from the user
     via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid. 
@@ -25,7 +24,7 @@ def get_sales_data():
         print("Data should be six numbers, searated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
         
         # In order to create a list that separate each value after the comma we use the split() method.
         sales_data = data_str.split(",")
@@ -40,7 +39,7 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    Inside the try, converts all strings values into integers.
+    Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int, 
     or if there aren't exactly 6 values.
     """
@@ -65,7 +64,7 @@ def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet update successfully.\n")
+    print(f"Worksheet {worksheet} updated successfully.\n")
 
 
 def calculate_surplus_data(sales_row):
